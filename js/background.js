a264e475efcf03a1185719ec488e439f86950f33
seungwonme <seungwonan.me@gmail.com>
2023-01-16 11:39:58 +0900

    remove historys of background.js and weather.js

---
+++ js/background.js
@@ -0,0 +1,23 @@
+setTimeout(function () {
+    document.getElementById('loading').classList.add('end')
+},1000)
+
+setTimeout(function () {
+    document.getElementById('loading').classList.add('hidden')
+},2000)
+
+const Access_Key = config.UNSPLASH_KEY;
+const bgImgRun = (position) => {
+    const url = `https://api.unsplash.com/search/photos?page=1&query=1&client_id=${Access_Key}&orientation=landscape&per_page=20`;
+    fetch(url).then(response => response.json()).then(data => {
+        const random = Math.floor(Math.random()*20);
+        const source =  data.results[random].urls.full
+        document.body.style.background = `url(${source})`
+        document.body.style.backgroundSize = 'cover'
+    })
+}
+const bgImgError = () => {
+    console.log('Error');
+}
+
+navigator.geolocation.getCurrentPosition(bgImgRun,bgImgError)
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = config.WEATHER_KEY
+
+const weatherName = document.querySelector('#weather span')
+const weatherStatus = document.querySelector('#weather span:last-child')
+
+const onGeoRun = (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const name = data.name;
+        const weather = data.weather[0].main
+        weatherName.innerText = name
+        weatherStatus.innerText = weather
+    })
+}
+const onGeoError = () => {
+    console.log('Error');
+}
+
+navigator.geolocation.getCurrentPosition(onGeoRun,onGeoError)
