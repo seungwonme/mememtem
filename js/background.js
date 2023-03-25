0a9955c8d56f679f896e22f2fc8a2c527cf1d212
Seungwon <seungwonan.me@gmail.com>
2023-03-25 21:24:34 +0900

    수정수정

--- js/background.js
+++ js/background.js
@@ -6,9 +6,9 @@ setTimeout(function () {
     document.getElementById('loading').classList.add('hidden')
 },2000)
 
-// const Access_Key = config.UNSPLASH_KEY;
+const Access_Key = "7NYT2NgLdlfwqDTFiuDO1cGGRkM_vdXjgYuczj8oWZQ";
 const bgImgRun = (position) => {
-    const url = `https://api.unsplash.com/search/photos?page=1&query=1&client_id=${UNSPLASH_KEY}&orientation=landscape&per_page=20`;
+    const url = `https://api.unsplash.com/search/photos?page=1&query=1&client_id=${Access_Key}&orientation=landscape&per_page=20`;
     fetch(url).then(response => response.json()).then(data => {
         const random = Math.floor(Math.random()*20);
         const source =  data.results[random].urls.full
--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-// const API_KEY = config.WEATHER_KEY
+const API_KEY = "94bef99a8f8c5524da10796744d95df5"
 
 const weatherName = document.querySelector('#weather span')
 const weatherStatus = document.querySelector('#weather span:last-child')
@@ -6,7 +6,7 @@ const weatherStatus = document.querySelector('#weather span:last-child')
 const onGeoRun = (position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then(response => response.json()).then(data => {
         const name = data.name;
         const weather = data.weather[0].main
